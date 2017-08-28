479df90add0e0992f043573202f9b9d19ba0d831
Avleen <avleen3@gmail.com>
2017-08-28 21:34:50 +0000

    game reset

--- game.rb
+++ game.rb
@@ -7,5 +7,28 @@ p2Name = gets.chomp
 require_relative 'question'
 require_relative 'player'
 
-player1 = Player.new(p1Name)
-player2 = Player.new(p2Name)
+p1 = Player.new(p1Name)
+p2 = Player.new(p2Name)
+
+loop do
+  #game reset
+  currentPlayer = 1
+  p1.reset
+  p2.reset
+
+  loop do
+    break unless (p1.is_alive && p2.is_alive)
+    player = (currentPlayer == 1) ? p1 : p2
+
+    question = Question.new()
+    puts "#{player.name}: #{question.print}"
+    ans = gets.chomp.to_i
+    if ans == question.answer
+      player.score += 1
+    else
+      player.lives -= 1
+      puts "#{player1.name} has #{player1.lives} lives and #{player2.name} has #{player2.lives} lives"
+    end
+
+    currentPlayer = (currentPlayer + 1) % 2
+  end
