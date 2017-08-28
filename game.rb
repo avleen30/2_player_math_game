04d09bb6ccaf2e25da524aec5315b0b561d9b334
Avleen <avleen3@gmail.com>
2017-08-28 21:38:58 +0000

    game end

--- game.rb
+++ game.rb
@@ -1,7 +1,7 @@
 puts "Hello and welcome to this fun math game"
-prints "Player 1 : Please enter your name"
+print "Player 1 : Please enter your name "
 p1Name = gets.chomp
-prints "Player 2 : Please enter your name"
+print "Player 2 : Please enter your name "
 p2Name = gets.chomp
 
 require_relative 'question'
@@ -27,8 +27,24 @@ loop do
       player.score += 1
     else
       player.lives -= 1
-      puts "#{player1.name} has #{player1.lives} lives and #{player2.name} has #{player2.lives} lives"
+      puts "#{p1.name} has #{p1.lives} lives and #{p2.name} has #{p2.lives} lives"
     end
 
     currentPlayer = (currentPlayer + 1) % 2
   end
+
+  # End of game.
+  if p1.lives == 0
+    puts "#{p1.name} has lost!"
+  else
+    puts "#{p2.name} has lost!"
+  end
+
+  puts "#{p1.name}'s score is #{p1.score} and #{p2.name}'s score is #{p2.score}"
+
+  print 'Type yes to play again or any key to end the game: '
+  restart = gets.chomp
+  break unless restart == "yes"
+end
+
+puts "Game Over!"
