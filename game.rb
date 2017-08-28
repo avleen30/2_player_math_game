6d99b1bb046eca02fe03b0ba01677190dafdb4d8
Avleen <avleen3@gmail.com>
2017-08-28 21:26:53 +0000

    first commit

--- game.rb
+++ game.rb
@@ -1,1 +1,8 @@
-game.rb
+puts "Hello and welcome to this fun math game"
+prints "Player 1 : Please enter your name"
+p1Name = gets.chomp
+prints "Player 2 : Please enter your name"
+p2Name = gets.chomp
+
+require_relative 'question'
+require_relative 'player'
